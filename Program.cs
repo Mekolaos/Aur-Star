dbe058e1d3ecf6822a1793f85fb17bfc14cdd1a3
Mekolaos <Mekolaos@gmail.com>
2020-07-12 14:10:05 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aur_Star
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
